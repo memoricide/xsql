8a8becd752cb274a9153390dc6ba467ed7d13cb9
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-04-03 12:28:15 +0200

    WIP Commit, time to sleep.

--- lib/table.ex
+++ lib/table.ex
@@ -1,11 +1,26 @@
 defmodule XSQL.Table do
   def __struct__ do
-    %{ fields:      nil,
+    %{ name:        nil,
+       fields:      nil,
        constraints: nil,
        tablespace:  nil,
-       arguments:   nil,
+       args:        nil,
        specifiers:  nil,
        types:       nil,
-       params:      nil }
+       params:      nil,
+       context:     nil }
   end
+
+  defmacro table(name, props) when is_binary(name) and is_list(props) do
+    IO.inspect name
+    IO.inspect props
+    quote do
+      if unquote(props)[:fields] do
+        IO.puts "Full specification of a table"
+      else
+        IO.puts "Field-only specification of a table"
+      end
+    end
+  end
+
 end
