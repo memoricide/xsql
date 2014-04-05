f34a461061222c6cac8bc05a7250ae293013bbbb
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-04-05 22:27:20 +0200

    Remove unneeded stuff

--- lib/xsql.ex
+++ lib/xsql.ex
@@ -8,18 +8,6 @@ defmodule XSQL do
   end
 
   defmacro __using__(opts) do
-    hostname = Keyword.get(opts, :hostname, "localhost")
-    database = Keyword.get(opts, :database, "xsql")
-    username = Keyword.get(opts, :username, "xsql")
-    password = Keyword.get(opts, :password, "h4xm3")
-    pools = Keyword.get(opts, :pools, [{XSQL.Pool, [
-                                          size: 10,
-                                          max_overflow: 10
-                                        ], [
-                                          hostname: hostname,
-                                          database: database,
-                                          username: username,
-                                          password: password ]}])
     sqltools = [XSQL.Table, XSQL.Column, XSQL.Constraint, XSQL.Type]
     tools = for tool <- sqltools do
       quote do
