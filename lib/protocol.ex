f06ef10ca0736979ff50e0e773bf8241e6c6390a
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-04-05 13:03:58 +0200

    Implement basic table macro

--- lib/protocol.ex
+++ lib/protocol.ex
@@ -1,3 +1,11 @@
 defprotocol XSQL.Protocol do
   def to_sql(thing)
 end
+
+for toimpl <- [ BitString, Integer, Float ] do
+  quote do 
+    defimpl XSQL.Protocol, for: unquote(toimpl) do
+      def to_sql(x), do: x |> XSQL.Util.to_binary
+    end
+  end
+end
--- lib/table.ex
+++ lib/table.ex
@@ -1,14 +1,18 @@
 defmodule XSQL.Table do
   def __struct__ do
-    %{ name:        nil,
-       fields:      nil,
-       constraints: nil,
-       tablespace:  nil,
-       args:        nil,
-       specifiers:  nil,
-       types:       nil,
-       params:      nil,
-       context:     nil }
+    %{ scope:       nil,
+       temp:        nil,
+       unlogged:    nil,
+       new:         true,
+       name:        nil,
+       of:          nil,
+       columns:     nil,
+       params:      nil, }
+       #with:        nil,
+       #on_commit:   nil,
+       #tablespace:  nil,
+       #like:        nil,
+       #constraints: nil }
   end
 
   defmacro table(name, props) when is_binary(name) and is_list(props) do
@@ -16,13 +20,15 @@ defmodule XSQL.Table do
     IO.inspect props
     quote do
       ps = unquote(props)
-      if Keyword.has_key? ps, :fields do
-        IO.puts "Full specification of a table"
-        IO.inspect Enum.into(ps, %unquote(__MODULE__){})
+      table =
+      if Keyword.has_key? ps, :columns do
+        %unquote(__MODULE__){} |> 
+          Map.merge(Enum.into(ps, %unquote(__MODULE__){}))
       else
-        IO.puts "Partial specification of a table"
-        IO.inspect %unquote(__MODULE__){fields: ps}
+        %unquote(__MODULE__){name: unquote(name), columns: ps}
       end
+      IO.inspect table
+      %unquote(__MODULE__){ table | name: unquote(name)}
     end
   end
 
@@ -34,4 +40,30 @@ defmodule XSQL.Table do
     end
   end
 
+end
+
+defimpl XSQL.Protocol, for: XSQL.Table do
+  def to_sql(t = %XSQL.Table{scope: scope, temp: temp, unlogged: unlogged, new: new, name: name, of: of, columns: columns, params: params}) do
+    buff = if scope,       do: ["CREATE", scope],             else: ["CREATE"]
+    buff = if temp,        do: buff ++ ["TEMP"],              else: buff
+    buff = if unlogged,    do: buff ++ ["UNLOGGED"],          else: buff
+    buff = if new,         do: buff ++ ["IF NOT EXISTS"],     else: buff
+    buff = if name,        do: buff ++ [ name ],              else: :erlang.error({XSQL.Table, "Name expected #{inspect t}"})
+    buff = if of,          do: buff ++ ["OF", of],            else: buff
+    buff = if columns,     do: buff ++ ["(", columns_to_sql(columns), ")"], else: buff
+    buff = if params,      do: buff ++ params_to_sql(params), else: buff
+    buff |> Enum.join(" ")
+  end
+
+  defp columns_to_sql(fs) do
+    IO.inspect(fs)
+    fs |>
+    Enum.map(fn(x) -> XSQL.Protocol.to_sql(x) end) |>
+    Enum.join ","
+  end
+  defp params_to_sql(ps) do
+    ps |>
+    Enum.map(fn(x) -> XSQL.Protocol.to_sql(x) end) |> 
+    Enum.join " "
+  end
 end
